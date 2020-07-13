7619c86b3ab72e3bcad38c70fd43c0d1cb43ab5e
Toby Sykes <toby.sykes@Tobys-MacBook-Pro.local>
2020-07-13 16:37:43 +0100

    forgot js

--- Website-dockerfiles/toby-site/website-code/inc/nav.php
+++ Website-dockerfiles/toby-site/website-code/inc/nav.php
@@ -1,7 +1,7 @@
 <nav class="navbar navbar-expand-lg navbar-dark" style="background-color: #2F2143;">
   <a class="navbar-brand" href="/">
     <!-- Page logo and name -->
-    <img src="/img/tobes.png" width="30" height="30" alt="eko"> tobes
+    <img src="/img/tobes.png" width="100" height="100" alt="tobes logo"> tobes
   </a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
@@ -9,11 +9,11 @@
   <div class="collapse navbar-collapse" id="navbarNav">
     <ul class="navbar-nav">
     <li class="nav-item">
-        <a class="nav-link" href="/">Home <span class="sr-only">(current)</span></a>
+        <a class="nav-link" href="/">home <span class="sr-only">(current)</span></a>
       </li>
       <li class="nav-item dropdown">
         <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-          AP
+          menu
         </a>
         <div class="dropdown-menu" aria-labelledby="navbarDropdown">
           <a class="dropdown-item" href="../pgs/guitar.php">Guitar</a>
