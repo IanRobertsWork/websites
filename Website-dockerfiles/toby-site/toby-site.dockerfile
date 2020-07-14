5a2e88ac4dcb378a85bf9cacc145c54ba4b4b4b2
Toby Sykes <toby.sykes@tobys-mbp.home>
2020-07-14 13:47:43 +0100

    risky business

--- Website-dockerfiles/toby-site/toby-site.dockerfile
+++ Website-dockerfiles/toby-site/toby-site.dockerfile
@@ -1,3 +1,9 @@
 FROM php:7.2-apache
 
+RUN apt-get update
+
+RUN apt-get install vim
+
 COPY /website-code/ /var/www/html/
+
+COPY /website-code/000-default.conf /etc/apache2/sites-enabled/
--- Website-dockerfiles/toby-site/website-code/index.php
+++ Website-dockerfiles/toby-site/website-code/index.php
@@ -3,26 +3,8 @@
 
 <body>
     <div class="container">
-        <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
-            <ol class="carousel-indicators">
-                <li data-target="#carouselExampleCaptions" data-slide-to="0" class="active"></li>
-                <li data-target="#carouselExampleCaptions" data-slide-to="1"></li>
-            </ol>
-            <div class="carousel-inner">
-                <div class="carousel-item active">
-                <img src="/img/fry.jpg" id="fry" class="d-block w-100" data-interval="1000" alt="fry">
-                    <div class="carousel-caption d-none d-md-block">
-                        <h5 class="text-dark">not sure what the purpose of this is</h5>
-                    </div>
-                </div>
-                <div class="carousel-item">
-                <img src="/img/fry_copy.jpg" id="fry" class="d-block w-100" data-interval="1000" alt="fry?">
-                    <div class="carousel-caption d-none d-md-block">
-                        <h5 class="text-dark">maybe if I look this way</h5>
-                    </div>
-                </div>
-            </div>
-        </div>
+        <img src="img/fry.jpg" id="homescreen" alt="fry">
+    </div>
 </body>
 
 <?php include "inc/footer.php"; ?>
